9d90f965c5f6ede6a2e3e31e0fa484b5e551a45e
yahir140306 <joseyahirhernandez11@gmail.com>
2025-05-09 22:08:26 -0600

    Añade la interfaz Task y actualiza home.component para gestionar tareas con propiedades de id, título y estado completado.

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,5 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { Component, signal } from '@angular/core';
+import { Task } from '../../models/task.model';
 
 @Component({
   selector: 'app-home',
@@ -9,10 +10,51 @@ import { Component, signal } from '@angular/core';
   styleUrl: './home.component.css',
 })
 export class HomeComponent {
-  tasks = signal([
-    'Instalar Angular CLI',
-    'Crear nuevo proyecto',
-    'Crear componentes',
-    'Crear servicios',
+  tasks = signal<Task[]>([
+    {
+      id: Date.now(),
+      title: 'Crear proyecto',
+      completed: false,
+    },
+    {
+      id: Date.now(),
+      title: 'Crear componente',
+      completed: false,
+    },
   ]);
+
+  changeHandler(event: Event) {
+    const input = event.target as HTMLInputElement;
+    const newTask = input.value;
+    this.addTask(newTask);
+  }
+
+  addTask(title: string) {
+    const newTask = {
+      id: Date.now(),
+      title,
+      completed: false,
+    };
+    this.tasks.update((tasks) => [...tasks, newTask]);
+  }
+
+  deleteTask(index: number) {
+    this.tasks.update((tasks) =>
+      tasks.filter((task, position) => position !== index)
+    );
+  }
+
+  updateTask(index: number) {
+    this.tasks.update((tasks) => {
+      return tasks.map( (task, position) => {
+        if (position === index) {
+          return {
+            ...task,
+            completed: !task.completed
+          }
+        }
+        return task;
+      })
+    })
+  }
 }
