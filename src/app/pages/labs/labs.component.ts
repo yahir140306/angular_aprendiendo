2e56188cade3880e39d6df4d787768fe887312c7
yahir140306 <joseyahirhernandez11@gmail.com>
2025-08-24 23:24:00 -0600

    Add width control to labs.component; implement dynamic width display for box based on user input

--- src/app/pages/labs/labs.component.ts
+++ src/app/pages/labs/labs.component.ts
@@ -33,6 +33,9 @@ export class LabsComponent {
   });
 
   colorCtrl = new FormControl();
+  widthCtrl = new FormControl(50, {
+    nonNullable: true,
+  });
 
   constructor() {
     this.colorCtrl.valueChanges.subscribe((value) => {
