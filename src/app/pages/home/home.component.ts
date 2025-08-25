86303dfa65f380aeb3a615b0431d80951c3ecea5
yahir140306 <joseyahirhernandez11@gmail.com>
2025-08-25 00:29:03 -0600

    Implement task editing functionality in home component; add methods to update editing mode and task text

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -74,5 +74,46 @@ export class HomeComponent {
         return task;
       });
     });
+
+    // this.tasks.mutate((state) => {
+    //   const currentTask = state[index];
+    //   state[index] = {
+    //     ...currentTask,
+    //     completed: !currentTask.completed,
+    //   };
+    // });
+  }
+
+  updateTaskEditingMode(index: number) {
+    this.tasks.update((tasks) => {
+      return tasks.map((task, position) => {
+        if (position === index) {
+          return {
+            ...task,
+            editing: true,
+          };
+        }
+        return {
+          ...task,
+          editing: false,
+        };
+      });
+    });
+  }
+
+  updateTaskText(index: number, event: Event) {
+    const input = event.target as HTMLInputElement;
+    this.tasks.update((tasks) => {
+      return tasks.map((task, position) => {
+        if (position === index) {
+          return {
+            ...task,
+            title: input.value,
+            editing: false,
+          };
+        }
+        return task;
+      });
+    });
   }
 }
